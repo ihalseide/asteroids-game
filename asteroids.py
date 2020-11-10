5a5f5889b11878e7ed83b31d7ba08843583fb06a
ihalseide <imitatio-ignotum@protonmail.com>
2020-11-10 14:38:55 -0700

    Formatted code.

--- asteroids.py
+++ asteroids.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 
-'''
-Asteroids clone in Python3 with pygame
-- Izak Halseide
-'''
-
 import sys
 import pygame as pg
 from src.main import main
 
 if __name__ == '__main__':
-	main()
-	pg.quit()
+    main()
+    pg.quit()
 
 sys.exit()
